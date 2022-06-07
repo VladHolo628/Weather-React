e9555b121ef54b656f5018833b09a9fc2da8dfdf
Vlad Holo <holovlad@yandex.ru>
2022-06-07 22:10:18 +1000

    Added basic tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,19 +1,33 @@
+import { useState } from 'react'
+import LocationScreen from '../LocationScreen/LocationScreen'
+
 function Tabs() {
+   const [currentTab, setCurrentTab] = useState('Now')
+
+
+ let clickHandler = (e,tab) => {
+    setCurrentTab(tab)
+ }
+
+ const buttons = ['Now','Details','Forecast']
 
     return (
+      <>
+        <LocationScreen screen={currentTab}></LocationScreen>
         <div className="tabs">
         <ul className="tabs__list">
-          <li className="tabs__item">
-            <button className="tabs__button tabs__button--active">Now</button>
-          </li>
-          <li className="tabs__item">
-            <button className="tabs__button">Details</button>
-          </li>
-          <li className="tabs__item">
-            <button className="tabs__button">Forecast</button>
-          </li>
+          {buttons.map((button) => {
+            const classes = button === currentTab?'tabs__button tabs__button--active':'tabs__button'
+            return (
+              <li className="tabs__item" >
+                <button  onClick={(e) => clickHandler(e,button)} className={classes}>{button}</button>
+              </li>
+            )
+
+          })}
         </ul>
       </div>
+      </>
     )
    
   
